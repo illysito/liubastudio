91fc91e7844c373fb20fecffee9781e51bb28789
illysito <illyalukyanovdesign@gmail.com>
2025-03-25 17:49:23 +0000

    Update motto.js

--- src/features/scripts/motto.js
+++ src/features/scripts/motto.js
@@ -3,18 +3,23 @@ import SplitType from 'split-type'
 
 function motto() {
   const motto_line = document.querySelectorAll('.motto-h-line')
+  const motto_line_2 = document.querySelectorAll('.motto-h-line-transp')
   // const motto_h = document.querySelector('.motto-h')
   const splitHeading = new SplitType(motto_line, { types: 'chars' })
+  const splitHeading2 = new SplitType(motto_line_2, { types: 'chars' })
+  console.log(splitHeading2)
 
   gsap.set(splitHeading.chars, {
-    opacity: 0.05,
-  })
-  gsap.set(motto_line, {
     opacity: 0,
+    xPercent: -120,
+    scale: 0.9,
+  })
+  gsap.set([motto_line, motto_line_2], {
+    opacity: 0.05,
     yPercent: 100,
   })
 
-  gsap.to(motto_line, {
+  gsap.to([motto_line, motto_line_2], {
     opacity: 1,
     yPercent: 0,
     duration: 1.4,
@@ -28,11 +33,13 @@ function motto() {
   })
   gsap.to(splitHeading.chars, {
     opacity: 1,
+    xPercent: 0,
+    scale: 1,
     duration: 0.5,
     scrollTrigger: {
       trigger: motto_line,
       start: 'top bottom',
-      end: 'top 8%',
+      end: 'top 16%',
       scrub: 2,
       markers: false,
     },
