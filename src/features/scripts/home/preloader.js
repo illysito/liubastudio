a24316519e5dabf814d3bd023f6ea31bdb75acf6
illysito <illyalukyanovdesign@gmail.com>
2025-12-13 19:44:07 +0000

    :)

--- src/features/scripts/home/preloader.js
+++ src/features/scripts/home/preloader.js
@@ -8,34 +8,34 @@ function preloader() {
   const TXT = $('.preloader-txt')
 
   let counter = 0
-  if (localStorage.getItem('preloaderShown') !== 'true') {
-    const interval = setInterval(() => {
-      counter += 2
-      TXT.textContent = `${counter} %`
-      if (counter == 100) {
-        const SPLIT_TXT = new SplitType(TXT, {
-          types: 'chars',
-          tagName: 'span',
-        })
-        gsap.to(SPLIT_TXT.chars, {
-          yPercent: -100,
-          stagger: 0.08,
-          duration: 1,
-          ease: 'power2.inOut',
-          onComplete: () => {
-            gsap.to(PRELOADER, {
-              opacity: 0,
-              zIndex: -100,
-              duration: 1.2,
-              ease: 'power2.inOut',
-            })
-            localStorage.setItem('preloaderShown', 'true')
-          },
-        })
-        clearInterval(interval)
-      }
-    }, 40)
-  }
+  // if (localStorage.getItem('preloaderShown') !== 'true') {
+  const interval = setInterval(() => {
+    counter += 2
+    TXT.textContent = `${counter} %`
+    if (counter == 100) {
+      const SPLIT_TXT = new SplitType(TXT, {
+        types: 'chars',
+        tagName: 'span',
+      })
+      gsap.to(SPLIT_TXT.chars, {
+        yPercent: -100,
+        stagger: 0.08,
+        duration: 1,
+        ease: 'power2.inOut',
+        onComplete: () => {
+          gsap.to(PRELOADER, {
+            opacity: 0,
+            zIndex: -100,
+            duration: 1.2,
+            ease: 'power2.inOut',
+          })
+          // localStorage.setItem('preloaderShown', 'true')
+        },
+      })
+      clearInterval(interval)
+    }
+  }, 40)
+  // }
 }
 
 export default preloader
